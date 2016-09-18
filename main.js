ededb13d13dd2da549bf51d4fd6bf4f791c32dee
Luke Green <elukuent@gmail.com>
2016-09-17 20:18:20 -0700

    animated squares
    see above

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
 $(document).ready(function(){
   $('.first-square').on('click', function() {
-    var displayCheck = $('.black-square').css('display');
+    /*var displayCheck = $('.black-square').css('display');
     if (displayCheck === 'inline-block'){
       $('.black-square').css('display','none');
     } else {
       $('.black-square').css('display','inline-block');
-    }
+    }*/
+
+    $('.first-square, .second-square, .third-square, .fourth-square').css('transform','rotateY(360deg)');
 
   });
 })
